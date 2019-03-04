e17e3435a34bf292c033e64688fd52d6cd14031e
Asia Carter <asiamcarter@gmail.com>
2019-03-04 13:02:54 -0600

    setting up new console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
