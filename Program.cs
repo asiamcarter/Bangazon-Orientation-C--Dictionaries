37a4544a5a39c1140e0a2a31358f234dc4270b88
Asia Carter <asiamcarter@gmail.com>
2019-03-04 14:15:06 -0600

    working with dictionaries

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace c_dictionaries
 {
@@ -6,7 +7,49 @@ namespace c_dictionaries
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> languagesWithScore = new Dictionary<string, int>() {
+                {"English", 4},
+                {"Spanish", 0},
+                {"C#", 4}
+            };
+            //adding javascript, 3 key/value pair
+            languagesWithScore.Add("JavaScript", 3);
+            //adding French, 5 key/value pair
+            languagesWithScore["French"] = 5;
+            //changing the value of "spanish"
+            languagesWithScore["Spanish"] = 1;
+            PrintDictionary(languagesWithScore);
+            //grabbing the value of "French"
+            int frenchScore = languagesWithScore["French"];
+
+            //list that contains two dictionaries: languagesWithScore and languagesWithScore2
+            //in JavaScript this would be akin to two objects within an array
+            List<Dictionary<string, int>> langScoreList= new List<Dictionary<string, int>>();
+            langScoreList.Add(languagesWithScore);
+
+            Dictionary<string, int> languagesWithScore2 = new Dictionary<string, int> () {
+                {"English", 5}
+            };
+            langScoreList.Add(languagesWithScore2);
+            //iterating over the list that contains to dictionaries
+            Console.WriteLine("Printing LangeScoreList");
+            foreach(Dictionary<string,int> lang in langScoreList) {
+                PrintDictionary(lang);
+            }
+
+            //using ContainsValue for practice
+            if (languagesWithScore.ContainsValue(8)) {
+                Console.WriteLine("Yay!");
+            } else {
+                Console.WriteLine("BOO!");
+            }
+
+
+        }
+        static void PrintDictionary(Dictionary<string,int> dict) {
+            foreach(KeyValuePair<string, int> kvp in dict) {
+                Console.WriteLine($"key: {kvp.Key}, value: {kvp.Value}");
+            }
         }
     }
 }
